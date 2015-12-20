5e2b6923b9011064449039717ac3a77aadea18b0
ajlopez <webmaster@ajlopez.com>
2015-12-20 10:50:31 -0300

    Split by column value

--- test/info.js
+++ test/info.js
@@ -1,5 +1,6 @@
 
 var analyzer = require('../lib/analyzer');
+var sl = require('simplelists');
 
 var cases = [
     [ 'value11', 'value21', 'class1' ],
@@ -12,7 +13,6 @@ var cases = [
 exports['info cases'] = function (test) {
     var result = analyzer.info(cases, 2);
     
-    console.dir(result);
     test.ok(result);
     test.ok(result > 0);
 };
@@ -20,10 +20,37 @@ exports['info cases'] = function (test) {
 exports['info subcases'] = function (test) {
     var result = analyzer.infox(cases, 2);
 
-    console.dir(result);
     test.ok(result);
     test.equal(Object.keys(result).length, 2);
     test.ok(result[0] >= 0);
     test.ok(result[1] >= 0);
+};
+
+exports['info subcases'] = function (test) {
+    var result = analyzer.infox(cases, 2);
+
+    test.ok(result);
+    test.equal(Object.keys(result).length, 2);
+    test.ok(result[0] >= 0);
+    test.ok(result[1] >= 0);
+};
+
+exports['split by column value'] = function (test) {
+    var cases = [
+        [ 1, 'value21', 'class1' ],
+        [ 1, 'value22', 'class1' ],
+        [ 3, 'value21', 'class2' ],
+        [ 4, 'value22', 'class2' ],
+        [ 4, 'value23', 'class3' ]
+    ];
+    
+    var result = analyzer.splitval(cases, 0, 2);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 2);
+    
+    sl.all(result[0], function (item) { return item[0] <= 2 });
+    sl.all(result[1], function (item) { return item[0] > 2 });
 };
 
